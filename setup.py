f866208387bbe4781e6932fa4adb577b88cf0cc6
hinneburg <hinneburg@informatik.uni-halle.de>
2019-10-10 11:02:43 +0200

    sklearn not skylearn

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Collection of PRML algorithms",
     author="ctgk",
     python_requires=">=3.6",
-    install_requires=["numpy", "scipy", "jupyter", "matplotlib", "skylearn"],
+    install_requires=["numpy", "scipy", "jupyter", "matplotlib", "sklearn"],
     packages=find_packages(exclude=["test", "test.*"]),
     test_suite="test"
 )
