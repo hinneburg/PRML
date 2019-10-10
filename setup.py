b9d2d610f1f0362f699c07c17c70161821bdcf52
hinneburg <hinneburg@informatik.uni-halle.de>
2019-10-10 11:13:52 +0200

    require specific versions

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     description="Collection of PRML algorithms",
     author="ctgk",
     python_requires=">=3.6",
-    install_requires=["numpy", "scipy", "jupyter", "matplotlib", "sklearn"],
+    install_requires=["numpy", "scipy==1.2.0", "jupyter", "matplotlib", "scikit-learn==0.18.2"],
     packages=find_packages(exclude=["test", "test.*"]),
     test_suite="test"
 )
